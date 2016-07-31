9f5256f2083c15714e4044c9230e49ae1737b38e
Javier Quiroga <javaito@gmail.com>
2016-07-30 23:18:39 -0300

    Add stress http server testing

--- test/Main.java
+++ test/Main.java
@@ -1,3 +1,4 @@
+import org.hcjf.io.net.http.*;
 import org.hcjf.log.Log;
 import org.hcjf.properties.SystemProperties;
 
@@ -7,11 +8,32 @@ import org.hcjf.properties.SystemProperties;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Log.d("", "Hola mundo!!");
+//        Log.d("", "Hola mundo!!");
+//
+//        Log.d("", "Hola, excepcion!!", new NullPointerException());
+//
+//        SystemProperties.get("bla", V -> (V.length() == 4));
 
-        Log.d("", "Hola, excepcion!!", new NullPointerException());
 
-        SystemProperties.get("bla", V -> (V.length() == 4));
+        HttpServer server = new HttpServer(1338);
+        server.addContext(new Context(".*") {
+            @Override
+            public HttpResponse onContext(HttpRequest request) {
+                byte[] body = "Hello world!!".getBytes();
+                HttpResponse response = new HttpResponse();
+                response.setResponseCode(200);
+                response.setBody(body);
+                response.addHeader(new HttpHeader(HttpHeader.CONTENT_LENGTH, Integer.toString(body.length)));
+                return response;
+            }
+
+            @Override
+            protected HttpResponse onError(HttpRequest request, Throwable throwable) {
+                return null;
+            }
+        });
+        server.start();
+
     }
 
 }
