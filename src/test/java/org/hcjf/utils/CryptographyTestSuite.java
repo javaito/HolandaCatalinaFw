d8151edd788096abc6c0fefe85c7c7ac52b71628
armedina <armedina@gmail.com>
2018-07-31 11:36:43 -0300

    Se agrega IV aleatorio para cada valor encriptado

--- src/test/java/org/hcjf/utils/CryptographyTestSuite.java
+++ src/test/java/org/hcjf/utils/CryptographyTestSuite.java
@@ -10,14 +10,15 @@ import org.junit.Test;
 public class CryptographyTestSuite {
 
     private static final String message = "Las máquinas me sorprenden con mucha frecuencia";
-    private static final String encryptedMessage = "6b44b12578cf682eca96bb74f9fcef699cc6e37cdddcdff48e64ff8c511e04521ba3fdb5f009c57023319cb50b3d5938564bdb3eda58d59da2dc4c227b9d7bd7";
+    private static final String encryptedMessage = "b72159963a9a6a877f276a6fd08dfd920e1a532d3570b853a6103d0db8f28c100001fd776a28e9419eab7050c9cc2a56a9134f48fff2969643627cdc0a6f36c49826f27fb9834ddff8445346ca6b57c601edf583db62a1369864dfd539f81b0f46f672282ac36df0efde06845b6adef64393fb517009784fa2ca493e88b16feb713e9d24bca4ef163c64f3b82cc4f48e9a95b3c4f8d030a0448fa7ac8f8c99d3";
     byte[] test;
 
-    @Test
+    @Test(timeout = 20000)
     public void encrypt() {
         Cryptography crypt = new Cryptography();//(key,"RandomIVTestService","HolandaCatalinaCrypt","AES","GCM","PKCS5Padding",128);
-        test = crypt.encrypt(message.getBytes());
-        System.out.println(Strings.bytesToHex(test));
+        for (int i = 0; i < 1000000; i++) {
+            crypt.encrypt((message + i).getBytes());
+        }
     }
 
     @Test
