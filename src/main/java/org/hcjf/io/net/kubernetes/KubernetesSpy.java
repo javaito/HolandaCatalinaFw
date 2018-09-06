5a2b6a408369c4aed3f256db46d5c758942801e4
javaito <javier.quiroga@sitrack.com>
2018-09-06 16:13:31 -0300

    Refactoring storage layer

--- src/main/java/org/hcjf/io/net/kubernetes/KubernetesSpy.java
+++ src/main/java/org/hcjf/io/net/kubernetes/KubernetesSpy.java
@@ -6,11 +6,16 @@ import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1Pod;
 import io.kubernetes.client.models.V1PodList;
 import io.kubernetes.client.util.Config;
+import org.hcjf.io.net.http.HttpClient;
+import org.hcjf.io.net.http.HttpHeader;
+import org.hcjf.io.net.http.HttpResponse;
 import org.hcjf.log.Log;
 import org.hcjf.properties.SystemProperties;
 import org.hcjf.service.Service;
 import org.hcjf.service.ServiceSession;
 
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
@@ -82,16 +87,30 @@ public final class KubernetesSpy extends Service<KubernetesSpyConsumer> {
 
         @Override
         public void run() {
+
+            //KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token) #
+            //curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME
+
             ApiClient client = null;
             CoreV1Api api = null;
 
+            String token = null;
+            String url = null;
+
             try {
+                token = new String(Files.readAllBytes(
+                        SystemProperties.getPath(SystemProperties.Net.KubernetesSpy.TOKEN_FILE_PATH)));
+
+                url = String.format(SystemProperties.get(SystemProperties.Net.KubernetesSpy.REST_URL),
+                        System.getenv(SystemProperties.get(SystemProperties.Net.KubernetesSpy.MASTER_NODE_HOST)),
+                        System.getenv(SystemProperties.get(SystemProperties.Net.KubernetesSpy.MASTER_NODE_PORT)),
+                        System.getenv(SystemProperties.get(SystemProperties.Net.KubernetesSpy.HOST_NAME)));
+
                 //Try to connect with the cluster.
                 client = Config.defaultClient();
                 client.getHttpClient().setConnectTimeout(
                         SystemProperties.getLong(SystemProperties.Net.KubernetesSpy.CLIENT_CONNECTION_TIMEOUT), TimeUnit.MILLISECONDS);
-                Configuration.setDefaultApiClient(client);
-                api = new CoreV1Api();
+                api = new CoreV1Api(client);
             } catch (Exception ex) {
                 Log.e(SystemProperties.get(SystemProperties.Net.KubernetesSpy.LOG_TAG), "Unable to start kubernetes spy", ex);
             }
@@ -100,25 +119,33 @@ public final class KubernetesSpy extends Service<KubernetesSpyConsumer> {
                 while(!Thread.currentThread().isInterrupted()) {
                     try {
 
-                        //TODO: Add many others events in this task and methods to call into the observer for each new event
+                        System.out.println("############URL: " + url);
 
-                        //Spy all the pods into the kubernetes cluster
-                        V1PodList list = api.listPodForAllNamespaces(
-                                null, null, null,
-                                null, null, null, null,
-                                null, null);
-                        for (V1Pod pod : list.getItems()) {
-                            for(KubernetesSpyConsumer consumer : consumers) {
-                                if(consumer.getInitTimestamp() < pod.getMetadata().getCreationTimestamp().getMillis() &&
-                                        consumer.getFilter().filter(pod)) {
-                                    if(pod.getMetadata().getDeletionTimestamp() != null) {
-                                        consumer.onLostPod(pod);
-                                    } else {
-                                        consumer.onDiscoveryPod(pod);
-                                    }
-                                }
-                            }
-                        }
+                        HttpClient httpClient = new HttpClient(new URL(url));
+                        httpClient.addHttpHeader(new HttpHeader(HttpHeader.AUTHORIZATION,
+                                String.format(SystemProperties.get(SystemProperties.Net.KubernetesSpy.AUTHORIZATION_HEADER), token)));
+                        HttpResponse response = httpClient.request();
+                        System.out.println("#############Body" + new String(response.getBody()));
+
+//                        //TODO: Add many others events in this task and methods to call into the observer for each new event
+//
+//                        //Spy all the pods into the kubernetes cluster
+//                        V1PodList list = api.listPodForAllNamespaces(
+//                                null, null, null,
+//                                null, null, null, null,
+//                                null, null);
+//                        for (V1Pod pod : list.getItems()) {
+//                            for(KubernetesSpyConsumer consumer : consumers) {
+//                                if(consumer.getInitTimestamp() < pod.getMetadata().getCreationTimestamp().getMillis() &&
+//                                        consumer.getFilter().filter(pod)) {
+//                                    if(pod.getMetadata().getDeletionTimestamp() != null) {
+//                                        consumer.onLostPod(pod);
+//                                    } else {
+//                                        consumer.onDiscoveryPod(pod);
+//                                    }
+//                                }
+//                            }
+//                        }
                     } catch (Exception ex) {
                         Log.e(SystemProperties.get(SystemProperties.Net.KubernetesSpy.LOG_TAG), "Kubernetes spy task exception", ex);
                     }
