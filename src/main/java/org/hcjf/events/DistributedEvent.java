de75281c7bf06a6f42e08f0998d119aa5b6a3d23
javaito <javaito@gmail.com>
2020-04-03 09:08:48 -0300

    Adding private and broadcasting behavior into the distributed events

--- src/main/java/org/hcjf/events/DistributedEvent.java
+++ src/main/java/org/hcjf/events/DistributedEvent.java
@@ -6,4 +6,21 @@ import org.hcjf.utils.bson.BsonParcelable;
  * @author javaito
  */
 public interface DistributedEvent extends Event, BsonParcelable {
+
+    /**
+     * If the event is private sending only for an other replica of the same service.
+     * @return Private value.
+     */
+    default Boolean isPrivate() {
+        return false;
+    }
+
+    /**
+     * If the event is broadcasting then the event is sending far all the replicas.
+     * @return Broadcasting value.
+     */
+    default Boolean isBroadcasting() {
+        return false;
+    }
+
 }
