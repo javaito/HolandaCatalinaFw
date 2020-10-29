c225696ddcf90f4a9641b9b74978c20661572bf5
javaito <javaito@gmail.com>
2020-10-29 16:33:46 -0300

    Adding the capability to aggregate only one field for row in sum and product aggregate functions

--- src/main/java/org/hcjf/layers/query/functions/ProductAggregateFunctionLayer.java
+++ src/main/java/org/hcjf/layers/query/functions/ProductAggregateFunctionLayer.java
@@ -21,35 +21,44 @@ public class ProductAggregateFunctionLayer extends BaseQueryAggregateFunctionLay
         Collection result = resultSet;
         if(parameters.length >= 1) {
             try {
-                boolean group = true;
-                if(parameters.length >= 2) {
-                    group = (boolean) parameters[1];
-                }
-                boolean accumulate = false;
-                if(parameters.length == 3) {
-                    accumulate = (boolean) parameters[2];
-                }
-                Number accumulatedValue = 0;
-                for (Object row : resultSet) {
-                    accumulatedValue = accumulateFunction(accumulatedValue,
-                            new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.multiply(V))[1];
-                    if(!group) {
-                        if(accumulate) {
-                            ((Map) row).put(alias, accumulatedValue);
+                if(parameters.length == 1) {
+                    Number sumValue;
+                    for (Object row : resultSet) {
+                        sumValue = accumulateFunction(0,
+                                new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.multiply(V))[1];
+                        ((Map) row).put(alias, sumValue);
+                    }
+                } else {
+                    boolean group = true;
+                    if (parameters.length >= 2) {
+                        group = (boolean) parameters[1];
+                    }
+                    boolean accumulate = false;
+                    if (parameters.length == 3) {
+                        accumulate = (boolean) parameters[2];
+                    }
+                    Number accumulatedValue = 0;
+                    for (Object row : resultSet) {
+                        accumulatedValue = accumulateFunction(accumulatedValue,
+                                new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.multiply(V))[1];
+                        if (!group) {
+                            if (accumulate) {
+                                ((Map) row).put(alias, accumulatedValue);
+                            }
                         }
                     }
-                }
 
-                if(group) {
-                    Collection<JoinableMap> newResultSet = new ArrayList<>();
-                    JoinableMap count = new JoinableMap(new HashMap<>(), alias);
-                    count.put(alias, accumulatedValue);
-                    newResultSet.add(count);
-                    result = newResultSet;
-                } else {
-                    if(!accumulate) {
-                        for (Object row : resultSet) {
-                            ((Map) row).put(alias, accumulatedValue);
+                    if (group) {
+                        Collection<JoinableMap> newResultSet = new ArrayList<>();
+                        JoinableMap count = new JoinableMap(new HashMap<>(), alias);
+                        count.put(alias, accumulatedValue);
+                        newResultSet.add(count);
+                        result = newResultSet;
+                    } else {
+                        if (!accumulate) {
+                            for (Object row : resultSet) {
+                                ((Map) row).put(alias, accumulatedValue);
+                            }
                         }
                     }
                 }
--- src/main/java/org/hcjf/layers/query/functions/SumAggregateFunctionLayer.java
+++ src/main/java/org/hcjf/layers/query/functions/SumAggregateFunctionLayer.java
@@ -21,35 +21,44 @@ public class SumAggregateFunctionLayer extends BaseQueryAggregateFunctionLayer i
         Collection result = resultSet;
         if(parameters.length >= 1) {
             try {
-                boolean group = true;
-                if(parameters.length >= 2) {
-                    group = getParameter(1, parameters);
-                }
-                boolean accumulate = false;
-                if(parameters.length == 3) {
-                    accumulate = getParameter(2, parameters);
-                }
-                Number accumulatedValue = 0;
-                for (Object row : resultSet) {
-                    accumulatedValue = accumulateFunction(accumulatedValue,
-                            new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.add(V))[1];
-                    if(!group) {
-                        if(accumulate) {
-                            ((Map) row).put(alias, accumulatedValue);
+                if(parameters.length == 1) {
+                    Number sumValue;
+                    for (Object row : resultSet) {
+                        sumValue = accumulateFunction(0,
+                                new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.add(V))[1];
+                        ((Map) row).put(alias, sumValue);
+                    }
+                } else {
+                    boolean group = true;
+                    if (parameters.length >= 2) {
+                        group = getParameter(1, parameters);
+                    }
+                    boolean accumulate = false;
+                    if (parameters.length == 3) {
+                        accumulate = getParameter(2, parameters);
+                    }
+                    Number accumulatedValue = 0;
+                    for (Object row : resultSet) {
+                        accumulatedValue = accumulateFunction(accumulatedValue,
+                                new Object[]{resolveValue(row, parameters[0])}, (A, V) -> A.add(V))[1];
+                        if (!group) {
+                            if (accumulate) {
+                                ((Map) row).put(alias, accumulatedValue);
+                            }
                         }
                     }
-                }
 
-                if(group) {
-                    Collection<JoinableMap> newResultSet = new ArrayList<>();
-                    JoinableMap count = new JoinableMap(new HashMap<>(), alias);
-                    count.put(alias, accumulatedValue);
-                    newResultSet.add(count);
-                    result = newResultSet;
-                } else {
-                    if(!accumulate) {
-                        for (Object row : resultSet) {
-                            ((Map) row).put(alias, accumulatedValue);
+                    if (group) {
+                        Collection<JoinableMap> newResultSet = new ArrayList<>();
+                        JoinableMap count = new JoinableMap(new HashMap<>(), alias);
+                        count.put(alias, accumulatedValue);
+                        newResultSet.add(count);
+                        result = newResultSet;
+                    } else {
+                        if (!accumulate) {
+                            for (Object row : resultSet) {
+                                ((Map) row).put(alias, accumulatedValue);
+                            }
                         }
                     }
                 }
